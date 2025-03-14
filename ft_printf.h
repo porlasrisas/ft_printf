e7c5c7ee97ca6d23f5387215d5694eaae54573e3
guigonzal <guigonza@c1r5s3.42madrid.com>
2025-03-14 13:13:14 +0100

    ft_printf

---
+++ ft_printf.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guigonza <guigonza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/11 12:42:19 by guigonza          #+#    #+#             */
+/*   Updated: 2024/10/14 19:48:56 by guigonza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+# include <ctype.h>
+# include <limits.h>
+
+int		ft_printf(const char *formart, ...);
+int		ft_putchar_fd(char c, int fd);
+int		ft_putstr_fd(char *s, int fd);
+int		ft_putnbr_fd(int n, int fd);
+int		ft_putnbr_hex(unsigned long n, int uppercase);
+
+#endif
